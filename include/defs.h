79b8dc877153cb5fe01f46c82c1242f64a5645d3
cryphon <yvanroes@gmail.com>
2025-03-08 13:33:05 +0100

    moved defs to header

---
+++ include/defs.h
@@ -0,0 +1,13 @@
+#ifndef DEFS_H
+#define DEFS_H
+
+#define SCREEN_WIDTH 1200
+#define SCREEN_HEIGHT 800
+#define PI 3.141592653589
+#define MIN_DISTANCE 10000.0f
+
+
+extern const double G; // m^3 kg^-1 s^-2
+
+
+#endif // DEFS_H
